4249f3c2e86ae27c01093037dc1654eddce85eec
vecel <mateusz.karandys@gmail.com>
2025-10-05 12:33:13 +0200

    Fix

--- next.config.mjs
+++ next.config.mjs
@@ -5,12 +5,19 @@ const repo = "/survey-visualizer"
 const nextConfig = {
   basePath: repo,
   assetPrefix: repo,
-  output: "export",
+  // output: "export",
   images: {
     unoptimized: true
   },
   reactStrictMode: true,
-  distDir: "out/_next"
+  // async rewrites() {
+  //   return [
+  //     {
+  //      source: `/survey-visualizer/_next/data/:path*`,
+  //      destination: '/_next/data/:path*'
+  //     },
+  //   ]
+  // },
 };
 
 export default nextConfig;
