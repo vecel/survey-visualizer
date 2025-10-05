4e1c511505e89a9a744ba5ee22e0f09939d21122
vecel <mateusz.karandys@gmail.com>
2025-10-05 12:02:37 +0200

    Fix css loading

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const repo = "/survey-visualizer"
+
 const nextConfig = {
-  basePath: "/survey-visualizer",
-  assetPrefix: "/survey-visualizer",
+  basePath: repo,
+  assetPrefix: repo,
   output: "export",
+  images: {
+    unoptimized: true
+  },
   reactStrictMode: true,
 };
 
