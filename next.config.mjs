d85c50df07df224f723033caedc1954c7b492de9
vecel <mateusz.karandys@gmail.com>
2025-10-05 14:04:04 +0200

    Change to deploy on netlify

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,10 @@
 /** @type {import('next').NextConfig} */
-
-const repo = "/survey-visualizer"
-
 const nextConfig = {
-  basePath: repo,
-  assetPrefix: repo,
   output: "export",
   images: {
     unoptimized: true
   },
   reactStrictMode: true,
-  trailingSlash: true
 };
 
 export default nextConfig;
