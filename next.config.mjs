255784d0e4c114a5016eacd7eb7f330b6e547395
vecel <mateusz.karandys@gmail.com>
2025-10-05 10:43:47 +0200

    Add assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/survey-visualizer",
+  assetPrefix: "/survey-visualizer",
   output: "export",
   reactStrictMode: true,
 };
